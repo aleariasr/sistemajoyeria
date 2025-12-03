41e1298acefb2c0e90d5bb5dd839322096b0c5bf
Alejandro Arias <alejandroarias@Alejandros-MacBook-Air.local>
2025-12-03 16:46:22 -0600

    arreglar package

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
+// Load environment variables from backend/.env when running locally
+// This must run before other modules that read process.env
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
